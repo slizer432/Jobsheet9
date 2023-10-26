1b58c40daddead07d8bd6f4a8b0f4e241bca3b7f
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2023-10-26 17:14:35 +0700

    update percobaan 3 untuk menjawab pertanyaan

--- arrayRataNilai23.java
+++ arrayRataNilai23.java
@@ -3,6 +3,12 @@ public class arrayRataNilai23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiMhs = new int[10];
+        int lulus = 0;
+        int jumlahlulus = 0;
+        double rataLulus = 0;
+        int tidakLulus = 0;
+        int jumlahTidakLulus = 0;
+        double rataTidakLulus = 0;
         double total = 0;
         double rata2;
 
@@ -13,7 +19,22 @@ public class arrayRataNilai23 {
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
         }
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] > 70) {
+                jumlahlulus += nilaiMhs[i];
+                lulus++;
+            }
+            else
+            jumlahTidakLulus += nilaiMhs[i];
+            tidakLulus++;
+        }
         rata2 = total/nilaiMhs.length;
+        rataLulus = jumlahlulus / lulus;
+        rataTidakLulus = jumlahTidakLulus / tidakLulus;
+        System.out.println("Jumlah mahasiswa lulus = "+lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus = "+tidakLulus);
         System.out.println("Rata-rata nilai = "+rata2);
+        System.out.println("Rata-rata nilai lulus = "+rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = "+rataTidakLulus);
     }
 }
