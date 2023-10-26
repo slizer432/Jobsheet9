704a54d04fec9be2cab71b565323e347555ac434
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2023-10-26 15:44:29 +0700

    update percobaan 2 untuk menjawab pertanyaan

--- arrayNilai.java
+++ arrayNilai.java
@@ -3,12 +3,15 @@ public class arrayNilai {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-"+i+" : "+nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }else
+            System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
         }
     }
 }
